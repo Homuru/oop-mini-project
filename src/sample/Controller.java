f3704f90d784da34674203ecc5e95a4e3fbe9bb8
Homuru <quangdt2510@gmail.com>
2019-05-14 11:46:58 +0700

    Added Wall, Applying Force, Fixing Layout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,47 @@
 package sample;
 
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
 public class Controller {
 
+    @FXML
+    private Circle object;
+    @FXML
+    private Rectangle wall;
+    @FXML
+    private ToggleButton toggleWallBtn;
+    @FXML
+    private TextField massInput;
+    @FXML
+    private TextField forceInput;
+
+
+    @FXML
+    public void initialize() {
+        wall.setVisible(false);
+    }
+
+    public void toggleWall() {
+        if(toggleWallBtn.isSelected()) {
+            wall.setVisible(true);
+        } else {
+            wall.setVisible(false);
+        }
+    }
+
+    public void applyForce() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(3));
+        transition.setNode(object);
+        transition.setByX(100);
+
+        transition.play();
+    }
 }
---
+++ src/sample/Object.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Object {
+
+    private float mass;
+
+    public float getMass() {
+        return mass;
+    }
+
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+}
